1622dd3be2b74493699bc6729b9693a9b662a9f6
Bernardo <bernardoorosa@gmail.com>
2020-07-22 11:11:51 -0300

    :rocket: Mailgun config

--- services/mailer.service.js
+++ services/mailer.service.js
@@ -1,15 +1,16 @@
 const mailgun = require("mailgun-js");
 require('dotenv').config();
 const DOMAIN = process.env.MAILGUN_DOMAIN;
-const mg = mailgun({apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN});
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
 let User = require('../models/user.model');
+const MG_LOGIN = process.env.MAILGUN_SMTP_LOGIN;
 
 module.exports = function sendEmails(users){
     users.forEach(user => {
         User.findById(user.friend)
             .then((friend) =>{
                 let data = {
-                    from: "Mailgun Sandbox <postmaster@sandbox79d999baa29844bdbc54fcb15f87ece2.mailgun.org>",
+                    from:`${MAILGUN_SMTP_LOGIN}`,
                     to: user.email,
                     subject: "Seu amigo secreto foi sorteado!",
                     text: `Seu amigo secreto é ${friend.name} de email ${friend.email}. Prometemos não contar para ninguém`
